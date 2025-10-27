50a0eefa2a782b14472a102c05c053c886efcd1a
Hareesh123NH <nagasarihareesh2005@gmail.com>
2025-10-27 16:52:49 +0530

    add deployed backend url

--- src/components/axiosConfig.js
+++ src/components/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080", // ✅ backend base URL
+    baseURL: "https://booksale-backend-latest.onrender.com", // ✅ backend base URL
     headers: {
         "Content-Type": "application/json",
     },
